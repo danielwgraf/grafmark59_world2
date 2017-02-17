afa7056c617e5d278155c21f3b5e37f4f6918a01
Daniel Graf <grafmark59@gmail.com>
2017-02-17 10:26:04 -0500

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,5 +5,5 @@ class Article < ActiveRecord::Base
 	validates_presence_of :title, :content
 
 	scope :active, where('active = ?', true)
-        scope :alphabetical, order('title')
+	scope :alphabetical, order('title')
 end
